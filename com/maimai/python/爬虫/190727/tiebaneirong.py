6b35791e38e2b9f901b3f8b2c2f69bdfe54b7d0d
maidougit <maidousuiyue@163.com>
2019-07-31 09:04:05 +0800

    [update] 贴吧，爬虫

---
+++ com/maimai/python/爬虫/190727/tiebaneirong.py
@@ -0,0 +1,44 @@
+from lxml import html
+from multiprocessing.dummy import Pool as ThreadPool
+import  requests, json, sys,importlib
+
+#于defaultencoding来做转换。
+importlib.reload(sys)
+#sys.setdefaultencoding('utf-8')
+
+def towrite(contentdict):
+    f.writelines(u'回帖时间:' + str(contentdict['topic_play_time']) + '\n')
+    f.writelines(u'回帖内容:' + contentdict['topic_play_content'] + '\n')
+    f.writelines(u'回帖人:' + contentdict['user_name'] + '\n')
+
+
+def spider(url) :
+    urltext = requests.get(url)
+    selector = html.etree.HTML(urltext.text)
+    content_field = selector.xpath('//div[@class="l_post l_post_bright"]')
+    item = {}
+    for each in content_field:
+        replay_info = json.load(each.xpath('@data-field')[0].replace('&quot',''))
+        author = replay_info['author']['user_name']
+        content = '黎明'
+        replay_time = replay_info['content']['date']
+        print(content)
+        print(author)
+        print(replay_time)
+        item['user_name'] = author
+        item['topic_replay_time'] = replay_time
+        item['topic_replay_content'] = content
+        towrite(item)
+
+if __name__ == '__main__':
+    pool = ThreadPool(4)
+    f = open('F:\\python-test\\file\\tieba.txt', 'a')
+    page = []
+    for i in range(1, 21):
+        newpage = 'http://tieba.baidu.com/f?kw=python&ie=utf-8&pn=' + str(i)
+        page.append(newpage)
+
+    results = pool.map(spider ,page)
+    pool.close()
+    pool.join()
+    f.close()
